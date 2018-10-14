3e6630a4a1139c2b5c922a28020be965991270fe
jmichaels1 <michael.salr1590@gmail.com>
2018-10-14 03:48:33 +0200

    implementation Dto class

--- src/main/java/com/everis/bcn/dto/BookDto.java
+++ src/main/java/com/everis/bcn/dto/BookDto.java
@@ -1,5 +1,27 @@
 package com.everis.bcn.dto;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.everis.bcn.entity.Mesa;
+import com.everis.bcn.entity.Restaurant;
+import com.everis.bcn.entity.Turn;
+
+/**
+ * 
+ * @author J Michael
+ *
+ */
 public class BookDto {
+	
+	private ArrayList<Restaurant> aListRenstaurant;
+	private ArrayList<Turn> aListTurn;
+	
+	private int restaurantId;
+	private Date dia;
+	private int turnId;
+	private int persons;
+	private Mesa mesa;
 
 }
--- src/main/java/com/everis/bcn/dto/CancelDto.java
+++ src/main/java/com/everis/bcn/dto/CancelDto.java
@@ -1,5 +1,24 @@
 package com.everis.bcn.dto;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.everis.bcn.entity.Restaurant;
+import com.everis.bcn.entity.Turn;
+
+/**
+ * 
+ * @author J Michael
+ *
+ */
 public class CancelDto {
+	
+	private ArrayList<Restaurant> aListRenstaurant;
+	private ArrayList<Turn> aListTurn;
+	
+	private int restaurantId;
+	private Date dia;
+	private int turnId;
+	private int locator;
 
 }
