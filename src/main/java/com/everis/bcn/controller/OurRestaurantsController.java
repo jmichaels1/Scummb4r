7256d0084222779b566a8730eeccafcca8dc9d2b
jsalirio <jsalirio@BCN-7FCDZM2.usersad.everis.int>
2018-10-19 01:08:58 +0200

    add IRestaurantBusineeImp interface

--- src/main/java/com/everis/bcn/controller/OurRestaurantsController.java
+++ src/main/java/com/everis/bcn/controller/OurRestaurantsController.java
@@ -1,5 +1,17 @@
 package com.everis.bcn.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class OurRestaurantsController {
+	
+	@RequestMapping(value = "/restaurants", method = RequestMethod.GET)
+	public ModelAndView restaurants() {
+		ModelAndView model = new ModelAndView("restaurants");
+		return model;
+	}
 
 }
---
+++ src/main/java/com/veris/bcn/serviceImp/IResturantBusinessImp.java
@@ -0,0 +1,54 @@
+package com.veris.bcn.serviceImp;
+
+import java.util.ArrayList;
+
+import com.everis.bcn.entity.Booking;
+import com.everis.bcn.entity.Restaurant;
+import com.everis.bcn.entity.Turn;
+import com.everis.bcn.service.IResturantBusiness;
+
+public class IResturantBusinessImp implements IResturantBusiness {
+
+	@Override
+	public boolean editBokking(Booking booking) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean cancelBooking(Booking booking) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean reserve(Restaurant restaurant, Booking booking) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addRestaurant(Restaurant restaurant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Restaurant> getRestaurants() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Turn> getTurns() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Booking> getBookings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
